ff09e50db9bfa4ec4b737e5a9b4af25fb5ce8cc6
JangChangHwan <jch94@hanmail.net>
2016-03-08 02:15:09 +0900

    v1.5 released

--- GreenMulti.py
+++ GreenMulti.py
@@ -13,7 +13,6 @@ from threading import Thread
 import time
 import subprocess
 
-if time.time() > 1457148027 + (86400 * 26): sys.exit()
 
 class GreenMulti(wx.Frame, WebProcess):
 	def __init__(self, title):
@@ -104,6 +103,7 @@ class GreenMulti(wx.Frame, WebProcess):
 		try:
 			kbuid = self.Decrypt(self.ReadReg('kbuid'))
 			if not kbuid: kbuid = self.InputBox(u'넓은마을 로그인', u'아이디')
+
 			kbupw = self.Decrypt(self.ReadReg('kbupw'))
 			if not kbupw: kbupw = self.InputBox(u'넓은마을 로그인', u'비밀번호', pwd=1) 
 			if not kbuid or not kbupw: return self.msgbox(u'알림', u'사용자 아이디와 비밀번호는 필수 입력사항입니다.')
@@ -198,10 +198,16 @@ class GreenMulti(wx.Frame, WebProcess):
 			n = self.listctrl.GetFocusedItem()
 			if n == -1: return
 
+# 메일쓰기
 			if "mail.php" in self.lItemList[n][3] and "cmd=write" in self.lItemList[n][3]:
 				self.WriteMail(u"메일 쓰기", receiver="", retitle="", rebody="")
 				return
+# 동호회 회원가입
+			elif "club_join&mo=green" in self.lItemList[n][3]:
+				self.MemberJoin()
+				return
 
+# 일반적인 처리 시작
 			r = self.GetInfo(self.lItemList[n])
 			if r: 
 				self.DisplayItems(r)
@@ -560,25 +566,25 @@ class GreenMulti(wx.Frame, WebProcess):
 
 	def OnHelp(self, e):
 		try:
-			msg = """초록멀티 1.5 맛보기 버전 두번째
+			msg = u"""초록멀티 1.5
 제작자 : 장창환
-단축키 안내
-게시판 진입 : Enter
-될돌아 나오기 : Escape도 되고, BackSpace도 되고, Alt + LeftArrow 중 아무 것나 사용하세요.
+이메일 : 462356@gmail.com
+*** 단축키 안내 ***
+게시판 진입 : Enter 
+되돌아 나오기 : Escape도 되고, BackSpace도 되고, Alt + LeftArrow 중 편할 걸로 사용하세요.
 컨트롤 간의 이동: Tab 
 홈으로 이동 : Alt + Home
-다음 페이지 : Page Down
-이전 페이지 Page Up
-게시물 쓰기 : 영문 W
-게시물 수정하기 : 영문 E
-게시물 삭제하기 : Delete
-다운로드하기 : 영문 D
+다음 페이지 : Page Down / 이전 페이지 Page Up
+게시물 쓰기 : 영문 W / 게시물 수정하기 : 영문 E
+게시물 삭제하기 : Delete / 다운로드하기 : 영문 D
+목록 새로 고침 : F5
+게시물 검색하기 : 컨트롤 영문 F / 코드 바로가기 : 컨트롤 영문 G
+다운로드 폴더 열기: Control + 영문 O
 파일 전송 정보 보기 : Control + 영문 J
 파일 정보 보기 대화상자에서의 단축키
-정보 새로고침 : Space 혹은 Enter
-전송 취소 : Delete
-다운로드 폴더 열기: Control + 영문 O
-* 주의 : 이 프로그램은 3월 말까지 정상동작합니다."""
+정보 새로고침 : Space  
+전송 프로세스 갯수 보기 : Enter
+전송 취소 : Delete """
 
 			self.MsgBox(u"도움말", msg)
 		except:
@@ -668,9 +674,43 @@ class GreenMulti(wx.Frame, WebProcess):
 		except:
 				return True
 
+	def MemberJoin(self):
+		self.Get("http://web.kbuwel.or.kr/menu/index.php?cmd=club_join&mo=green&club=green&bcode=green99")
+		if self.html.find("history.go(-1)") >= 0:
+			return self.MsgBox(u"결과", u"이미 초록등대에 가입되어 있거나 가입 신청되어 있습니다.")
+
+		data = {}
+		url = "http://web.kbuwel.or.kr/menu/index.php?mo=green&club=green&bcode=green99&cmd=club_join_insert"
+
+		nickname = self.InputBox(u"별명 입력", u"동호회에서 사용할 별명이나 이름을 입력하세요.")
+		if not nickname: return self.MsgBox(u"오류", u"별명은 필수입니다. 회원 가입을 다시 해 주세요.")
+		data[u"닉네임".encode("euc-kr", "ignore")] = nickname.encode("euc-kr", "ignore")
+
+		name = self.InputBox(u"실명과 성별 입력", u"실명과 성별을 입력하세요.")
+		if not name: return self.MsgBox(u"오류", u"실명과 성별은 필수입니다. 회원 가입을 다시 해 주세요.")
+		data[u"답변1".encode("euc-kr", "ignore")] = name.encode("euc-kr", "ignore")
+
+		age = self.InputBox(u"나이와 지역 입력", u"나이와 지역을 입력하세요.")
+		if not age: return self.MsgBox(u"오류", u"나이와 지역은 필수입니다. 회원 가입을 다시 해 주세요.")
+		data[u"답변2".encode("euc-kr", "ignore")] = age.encode("euc-kr", "ignore")
+
+		motive = self.InputBox(u"가입 동기 입력", u"동호회에 가입하게 된 동기를 입력하세요.")
+		if not motive: return self.MsgBox(u"오류", u"가입 동기는 필수입니다. 회원 가입을 다시 해 주세요.")
+		data[u"답변3".encode("euc-kr", "ignore")] = motive.encode("euc-kr", "ignore")
+
+		if self.MsgBox(u"질문", u"2개월 이상 아무런 활동이 없다면 자동으로 회원 탈퇴됩니다. 동의합니까? 취소를 선택하면 회원 가입이 취소됩니다.", True) == False: return 
+		data[u"답변4".encode("euc-kr", "ignore")] = u"예, 동의합니다.".encode("euc-kr", "ignore")
+
+		if self.MsgBox(u"질문", u"동호회 자료는 반드시 동호회 내부에서만 공유되어야합니다. 외부로 유츌시 탈퇴처리됩니다. 동의합니까? 동의하지 않으면 가입이 취소됩니다.", True) == False: return 
+		data[u"답변5".encode("euc-kr", "ignore")] = u"예, 동의합니다.".encode("euc-kr", "ignore")
+
+		if self.MsgBox(u"가입 신청", u"정보 입력을 마쳤습니다. 초록등대에 가입 신청을 할까요?", True) == True:
+			self.Post(url, data)
+
+
 
 if __name__ == "__main__":
 	freeze_support()
 	app = wx.App()
-	f = GreenMulti(u"초록멀티 v1.5 RC")
+	f = GreenMulti(u"초록멀티 v1.5")
 	app.MainLoop()
